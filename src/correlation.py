f65c273520d664710ee7ca2366964303fe320156
Andrew Goering <andrew.goering1@gmail.com>
2025-08-01 14:00:27 -0400

    small modifications to make it work with newer versions of python libraries

--- src/correlation.py
+++ src/correlation.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import torch
-
 import cupy
 import math
 import re
@@ -274,8 +273,8 @@ def cupy_kernel(strFunction, objVariables):
 
 
 # end
-
-@cupy.util.memoize(for_each_device=True)
+#used to be cupy.util.memoize, Andy changed it
+@cupy.memoize(for_each_device=True)
 def cupy_launch(strFunction, strKernel):
     return cupy.cuda.compile_with_cache(strKernel).get_function(strFunction)
 
